929746dd0f7038ad6d1590d0fbc7662e58c7db9f
tsuyoshiwada <v23.b.wada@gmail.com>
2016-04-23 17:04:49 +0900

    Update oauth2 login

--- googleapi/routes/index.js
+++ googleapi/routes/index.js
@@ -2,15 +2,38 @@
 
 const express = require("express");
 const router = express.Router();
+const Google = require("googleapis");
+const GoogleAuth = require("google-auth-library");
 
+const SCOPES = ["https://www.googleapis.com/auth/drive.file"];
+const auth = new GoogleAuth();
+const oauth2Client = new auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URL
+);
 
 router.get("/", (req, res) => {
-  if (!req.session.token) {
-    res.redirect("/login");
+  const token = req.session.token;
+
+  if (token) {
+    res.json(token);
+
   } else {
-    res.send("TODO");
+    const url = oauth2Client.generateAuthUrl({
+      scope: SCOPES
+    });
+    res.send(url);
   }
 });
 
+router.get("/callback", (req, res) => {
+  oauth2Client.getToken(req.query.code, (err, token) => {
+    if (err) return console.log("Error while trying to retrieve access token", err);
+
+    req.session.token = token;
+    res.redirect("/");
+  });
+});
 
 module.exports = router;
--- googleapi/server.js
+++ googleapi/server.js
@@ -1,16 +1,13 @@
-"use strict";
+"use strcit";
 
 require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const session = require("express-session");
-
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-const app = express();
-app.set("view engine", "ejs");
-app.set("views", `${__dirname}/views`);
 app.use(cookieParser());
 app.use(session({
   secret: "keyboard cat",
@@ -24,7 +21,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use("/", require("./routes/index"));
-app.use("/login", require("./routes/login"));
+
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
