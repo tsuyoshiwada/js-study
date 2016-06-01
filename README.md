bf4ccdedbe659ca8b7f6bfd634e465150db94556
tsuyoshiwada <v23.b.wada@gmail.com>
2016-06-01 12:53:47 +0900

    Initial rss-feed

--- README.md
+++ README.md
@@ -2,5 +2,3 @@ other-js-sandbox
 ================
 
 JavaScriptの勉強用リポジトリ。
-
-* Event Delegation
---
+++ rss-feed/src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import fetch from "isomorphic-fetch";
+
+export default class App extends Component {
+  componentWillMount() {
+  }
+
+  render() {
+    return (
+      <div>App</div>
+    );
+  }
+}
